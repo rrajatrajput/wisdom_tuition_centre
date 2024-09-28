b7b0f316e6c050d68cf9414fb0dcdc31cec5756f
RrajatRajput <rajputrajat563@gmail.com>
2024-09-29 01:09:50 +0530

    added new design code

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function sendMail() {
         name: document.getElementById("name").value,
         phone: document.getElementById("phone").value,
         email: document.getElementById("email").value,
-        query: document.getElementById("query").value,
+        message: document.getElementById("message").value,
     };
     
 
@@ -20,7 +20,7 @@ function sendMail() {
             document.getElementById("name").value = "";
             document.getElementById("phone").value = "";
             document.getElementById("email").value = "";
-            document.getElementById("query").value = "";
+            document.getElementById("message").value = "";
             alert("Your message was sent successfully!");
         })
         .catch(err => {
