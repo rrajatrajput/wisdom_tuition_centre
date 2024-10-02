ef089845d88e0e5fc3136f8282664594b90074a5
RrajatRajput <rajputrajat563@gmail.com>
2024-10-02 14:45:57 +0530

    added new BG photo

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 function sendMail() {
-    var params = {
+    let parms = {
         name: document.getElementById("name").value,
         phone: document.getElementById("phone").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value,
-    };
+    }
 
-    const serviceID = "service_mnvao5i";
-    const templateID = "template_gnbprxo";
-
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            console.log("Success:", res);
-            document.getElementById("name").value = "";
-            document.getElementById("phone").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            alert("Your message was sent successfully!");
-        })
-        .catch(err => {
-            console.error("Failed to send message:", err);
-            alert("Failed to send message. Please try again later.");
-        });
+    emailjs.send("service_mnvao5i","template_gnbprxo" , params).then (alert("Email Sent !!"))
 }
